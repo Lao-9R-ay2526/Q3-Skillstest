8a848ac1dc69ca5e96b74e5b37fe24ee3eb400e8
Lao-9R-ay2526 <lao.lil@obmontessori.edu.ph>
2026-03-13 22:16:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+// calculation
+
+
+function getTotal(){
+
+let ticketPrice = 250;
+
+let tickets = document.getElementById("order-tickets").value;
+
+let total = tickets * ticketPrice;
+
+document.getElementById("totalcash").innerHTML = total + "$";
+
+}
+
 
